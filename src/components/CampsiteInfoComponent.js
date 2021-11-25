ddcbb81b06bd94308eef32c8ac30260be8680a81
Brent Blake <brentablake@gmail.com>
2021-11-25 11:27:03 -0600

    Week 3 Final

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderCampsite({campsite}) {
+    return(
+        <div className="col-md-5 m-1">
+            <Card>
+                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardBody>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments})  {
+    if(comments){
+    return (
+        <div class="col-md-5 m-1">
+    <       h4>Comments</h4>
+{
+    comments.map((comment) => {
+        return (
+        <p key={comment.id}>{comment.text}
+        <br/>
+        --{comment.author}, {new Intl.DateTimeFormat('en-us', {year: 'numeric', month:'short', date:'2-digit'}).format(new Date(Date.parse(comment.date)))}
+        </p>)
+    })
+}
+    </div>
+)
+    }
+    return <div/>
 }
 
 function CampsiteInfo(props) {
@@ -12,8 +40,18 @@ function CampsiteInfo(props) {
         return (
             <div className="container">
                 <div className="row">
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>{props.campsite.name}</h2>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.campsite.comments} />
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
         );
